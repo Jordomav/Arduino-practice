dd9efb2cb2ed53cd3808cbd60c2e459c1fc6f543
Jordomav <jordomav@gmail.com>
2016-07-30 20:25:27 -0700

    add to the counter every time through the loop then turn light in pin 13 on when counter reaches 25. Read users outside inputs into the Serial

--- sketch_jul29a/sketch_jul29a.ino
+++ sketch_jul29a/sketch_jul29a.ino
@@ -1,32 +1,65 @@
 int ledPin = 13;
 int test = 10;
 int i;
+int counter = 0;
+
+int incomingByte = 0;
 
 void setup() {
   // put your setup code here, to run once:
     pinMode(ledPin, OUTPUT);
-    
+//    Test with the serial and loging 
+    Serial.begin(9600);
+//    Serial.println("Here is a log to the Serial");
+    delay(1000);
 }
 
 void loop() {
-  
-  
 //  If check to see if the flashLight integer is equal to the number
 //    if (flashLight == 5){
 //        digitalWrite(ledPin, HIGH);
 //      }
+
 
 //  Flash the light in pin 13 every second
 //    digitalWrite(ledPin, HIGH);
 //    delay(1000);
 //    digitalWrite(ledPin, LOW);
 //    delay(1000);
-    
-outsideFunction();
-delay(1000);
-outsideFunction();
-delay(1000);
 
+
+// Use an outside function to make the 13th pin flash
+//outsideFunction();
+//delay(1000);
+//outsideFunction();
+//delay(1000);
+
+
+// Add one second to the counter integer every time it runs through the loop
+//counter++;
+//Serial.println(counter);
+//if (counter == 25) {
+//  counter = 0;
+//  }
+//  delay(1000);
+
+
+// Use the outside function to flash a light every time the counter reaches 25
+//counter++;
+//Serial.println(counter);
+//if (counter == 25) {
+//  counter = 0;
+//  outsideFunction();
+//  }
+//  delay(1000);
+
+
+// Write an input from the user into the serial
+if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.print("! ");
+    Serial.write(incomingByte);
+  }
 }
 
 void outsideFunction(){
